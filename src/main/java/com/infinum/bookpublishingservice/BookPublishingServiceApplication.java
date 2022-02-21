95943ac8eca576cf47accc0de4e53dbaae7101ff
stipe24 <51133162+stipe24@users.noreply.github.com>
2022-02-21 19:19:38 +0100

    empty project

---
+++ src/main/java/com/infinum/bookpublishingservice/BookPublishingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infinum.bookpublishingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPublishingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPublishingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinum/bookpublishingservice/BookPublishingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinum.bookpublishingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPublishingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
